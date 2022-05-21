a26e8490d79dd40464ac11dea599a20405ea016b
Roni <roni.c.castro@gmail.com>
2022-05-21 16:31:08 -0300

    feat: create base prisma config

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-async function start() {}
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.listen(3333, () => {
+  console.log('Server running');
+});
+
+app.get('/feedback', async (req, res) => {
+  const feedbacks = await prisma.feedbacks.findMany();
+  res.json(feedbacks);
+});
