d95d229d605ab39b3140935f252589b133443290
Roni <roni.c.castro@gmail.com>
2022-05-28 13:55:18 -0300

    fix: hardcoded port

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,6 @@ app.use(cors());
 app.use(express.json({ limit: '5mb' }));
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('Server running');
 });
